ed7be5a18acea3a57588a7f209c4db6dfad18990
Alex <muirkat138@gmail.com>
2021-10-05 17:04:17 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minLength = 8;
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "1234567890";
+            string specialChars = "~!@#$%^&*?";
+            Console.WriteLine("Please enter a password:");
+            string userPassword = Console.ReadLine();
+            int score = 0;
+            if(userPassword.Length >= minLength){
+                score ++;
+            }
+            if(Tools.Contains(userPassword, upperCase)){
+                score++;
+            }
+            Console.WriteLine(score);
+        }
+    }
+}
