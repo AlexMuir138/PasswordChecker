97ce60894b3b3769a06bcdb2e5907c4495f1c302
Alex <muirkat138@gmail.com>
2021-10-05 17:22:42 -0600

    finished

--- Program.cs
+++ Program.cs
@@ -15,12 +15,39 @@ namespace PasswordChecker
             string userPassword = Console.ReadLine();
             int score = 0;
             if(userPassword.Length >= minLength){
-                score ++;
+            score ++;
             }
             if(Tools.Contains(userPassword, upperCase)){
                 score++;
             }
-            Console.WriteLine(score);
+            if(Tools.Contains(userPassword, lowerCase)){
+                score++;
+            }
+            if(Tools.Contains(userPassword, digits)){
+                score++;
+            }
+            if(Tools.Contains(userPassword, specialChars)){
+                score++;
+            }
+            switch(score){
+                        case 5:
+                        case 4:
+                        Console.WriteLine("Strong AF");
+                        break;
+                        case 3:
+                        Console.WriteLine("strong like a child");
+                        break;
+                        case 2:
+                        Console.WriteLine("meh do better");
+                        break;
+                        case 1:
+                        Console.WriteLine("weak sauce");
+                        break;
+                        default:
+                        Console.WriteLine("No. Put a password in you dunce.");
+                        break;
+            }
+            
         }
     }
 }
